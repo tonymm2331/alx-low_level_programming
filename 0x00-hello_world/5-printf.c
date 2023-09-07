0ed9e33bbe910033b88b8cbbe807b3742ac0285b
Tony <tonymusyoka11@gmail.com>
2023-09-07 07:15:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("With proper grammer, but the out put is a pieces of art,\n");
+	return (0);
+}
