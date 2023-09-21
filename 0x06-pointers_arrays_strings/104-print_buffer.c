1ee1a5428960322a6b457f90392e0bf969bc3fd0
Tony <tonymusyoka11@gmail.com>
2023-09-21 05:44:21 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,54 @@
+#incude "main.h"
+
+
+void print_buffer(char *b, int size) {
+    if (size <= 0) {
+        printf("\n");
+        return;
+    }
+
+    int lineSize = 10;
+    int position = 0;
+
+    for (int i = 0; i < size; i += lineSize) {
+        printf("%08x ", position);
+
+        for (int j = 0; j < lineSize; j++) {
+            if (i + j < size) {
+                printf("%02x ", (unsigned char)b[i + j]);
+            } else {
+                printf("   ");
+            }
+
+            if (j == 4) {
+                printf(" ");
+            }
+        }
+
+        printf(" ");
+        for (int j = 0; j < lineSize; j++) {
+            if (i + j < size) {
+                char c = b[i + j];
+                if (isprint(c)) {
+                    printf("%c", c);
+                } else {
+                    printf(".");
+                }
+            } else {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+        position += lineSize;
+    }
+}
+
+int main() {
+    char buffer[] = "This is a sample buffer to print in hex and characters.";
+    int size = sizeof(buffer) - 1;
+
+    print_buffer(buffer, size);
+
+    return 0;
+}
