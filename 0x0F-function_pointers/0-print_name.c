22dbecfcb582a7775d53895381cbd3f85bbf2a1a
Tony <tonymusyoka11@gmail.com>
2023-10-11 06:03:13 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stdlib.h>
+
+void _putchar(char c);
 
 /**
- * print_name - prints a name
- *@f: function pointer
- * @name: name of the person
- *
- * Return: Nothing.
+ * print_name - Prints a name using a given function pointer.
+ * @name: The name to be printed.
+ * @f: A function pointer that specifies how to print the name.
  */
-
-void print_name(char *name, void (*f)(char *))
-{
-	if ((name == NULL) || (f == NULL))
-		return;
-	f(name);
+void print_name(char *name, void (*f)(char *)) {
+    if (name != NULL && f != NULL) {
+        while (*name) {
+            f(*name);
+            name++;
+        }
+        f('\n'); // Add a newline after printing the name.
+    }
 }
+
+
